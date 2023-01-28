2cbe21c83356f18f69f2a7add166562d5f72d498
Schoperation <cschop1@gmail.com>
2023-01-27 21:44:10 -0500

    Initial commit; basic bot

---
+++ main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/disgoorg/disgo"
+	"github.com/disgoorg/disgo/bot"
+	"github.com/disgoorg/disgo/discord"
+	"github.com/disgoorg/disgo/events"
+	"github.com/disgoorg/disgo/gateway"
+)
+
+func main() {
+	log.SetPrefix("SY")
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+
+	client, err := disgo.New("MTA2ODcyMDY5NDExODA3MjQzMA.GP6jeX.ImwFQfqRmYYBgT0Djyzzj6EhseAA4KJIcR95N8",
+		bot.WithGatewayConfigOpts(
+			gateway.WithIntents(
+				gateway.IntentGuildMessages,
+				gateway.IntentMessageContent,
+			),
+		),
+		bot.WithEventListenerFunc(onMessageCreate),
+	)
+	if err != nil {
+		log.Fatalf("Error while building the bot: %v", err)
+	}
+
+	defer client.Close(context.TODO())
+
+	err = client.OpenGateway(context.TODO())
+	if err != nil {
+		log.Fatalf("Error connecting to Discord gateway: %v", err)
+	}
+
+	log.Println("SchopYatch is up and running!")
+	s := make(chan os.Signal, 1)
+	signal.Notify(s, syscall.SIGINT, syscall.SIGTERM)
+	<-s
+}
+
+func onMessageCreate(event *events.MessageCreate) {
+	if event.Message.Author.Bot {
+		return
+	}
+
+	var message string
+	if event.Message.Content == "ping" {
+		message = "pong"
+	} else if event.Message.Content == "pong" {
+		message = "ping"
+	}
+
+	if message != "" {
+		_, _ = event.Client().Rest().CreateMessage(event.ChannelID, discord.NewMessageCreateBuilder().SetContent(message).Build())
+	}
+}
